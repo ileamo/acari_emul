5193406726258cdfd04fa2df79b72cc3140e393f
Igor Mosunov <imosunov@nsg.ru>
2019-03-05 19:10:26 +0300

    Run/stop real server

---
+++ run-real.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#Pull image
+docker pull ileamo/acari-server
+
+# Network
+docker network create acari-network-real
+
+# PostgreSQL
+docker run --rm -it \
+--name acari-server-real-db \
+--network acari-network-real \
+-e POSTGRES_PASSWORD=postgres \
+-e POSTGRES_DB=acari_server_prod \
+-v /var/lib/postgresql/docker/11.2/data:/var/lib/postgresql/data \
+-d postgres:11.2
+
+# Migrate & seed
+docker run --rm -it \
+--network acari-network-real \
+-e DB_HOST=acari-server-real-db \
+--cap-add=NET_ADMIN \
+ileamo/acari-server seed
+
+# Server
+docker run --rm -it \
+--name acari-server-real \
+--network acari-network-real \
+-e DB_HOST=acari-server-real-db \
+-p 51020:50020 \
+--cap-add=NET_ADMIN \
+--device /dev/net/tun:/dev/net/tun \
+-d ileamo/acari-server foreground
---
+++ stop-real.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker stop acari-server-real acari-server-real-db
+docker network rm acari-network-real
