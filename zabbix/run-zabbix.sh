6a7fcea7cd34c32fb3c923799653805de6a0fd4f
imosunov <imosunov@nsg.net.ru>
2019-03-23 15:09:54 +0300

    run zabbix main

--- zabbix/run-zabbix.sh
+++ zabbix/run-zabbix.sh
@@ -1,36 +1,35 @@
 #!/bin/sh
 
 docker run -t --rm \
-      --name zabbix-postgres-server \
+      --name zabbix-postgres-server-main \
       -e POSTGRES_USER="zabbix" \
       -e POSTGRES_PASSWORD="zabbix" \
-      -e POSTGRES_DB="zabbix_pwd" \
-      -v /var/lib/postgresql/docker/zabbix/data:/var/lib/postgresql/data \
+      -e POSTGRES_DB="zabbix" \
+      -v /var/lib/postgresql/docker/zabbix-main/data:/var/lib/postgresql/data \
       -v /etc/localtime:/etc/localtime:ro \
-      -d postgres:latest
+      -d postgres:11.2-alpine
 
 docker run -t --rm \
-      --name zabbix-server-pgsql \
-      -e DB_SERVER_HOST="zabbix-postgres-server" \
+      --name zabbix-server-pgsql-main \
+      -e DB_SERVER_HOST="zabbix-postgres-server-main" \
       -e POSTGRES_USER="zabbix" \
       -e POSTGRES_PASSWORD="zabbix" \
-      -e POSTGRES_DB="zabbix_pwd" \
-      --link zabbix-postgres-server:postgres \
-      -p 10051:10051 \
+      -e POSTGRES_DB="zabbix" \
+      --link zabbix-postgres-server-main:postgres \
+      -p 11051:10051 \
       -v /etc/localtime:/etc/localtime:ro \
       -d zabbix/zabbix-server-pgsql:latest
 
 docker run -t --rm \
-      --name zabbix-web-nginx-pgsql \
-      -e DB_SERVER_HOST="zabbix-postgres-server" \
+      --name zabbix-web-nginx-pgsql-main \
+      -e DB_SERVER_HOST="zabbix-postgres-server-main" \
       -e POSTGRES_USER="zabbix" \
       -e POSTGRES_PASSWORD="zabbix" \
-      -e POSTGRES_DB="zabbix_pwd" \
+      -e POSTGRES_DB="zabbix" \
       -e ZBX_SERVER_NAME="Zabbix NSG" \
-      --link zabbix-postgres-server:postgres \
-      --link zabbix-server-pgsql:zabbix-server \
-      -p 80:80 \
-      -p 10080:80 \
+      --link zabbix-postgres-server-main:postgres \
+      --link zabbix-server-pgsql-main:zabbix-server \
+      -p 11080:80 \
       -v /etc/ssl/nginx:/etc/ssl/nginx:ro \
       -v /etc/localtime:/etc/localtime:ro \
       -d zabbix/zabbix-web-nginx-pgsql:latest
