f9db0e04059800d91a402a2a6aa27a7562641e87
zabbix <zabbix@nsg.net.ru>
2019-03-23 22:17:52 +0300

    real main zabbix

--- zabbix/run-zabbix.sh
+++ zabbix/run-zabbix.sh
@@ -1,32 +1,36 @@
 #!/bin/sh
 
-docker run -t --rm \
+docker run -t \
       --name zabbix-postgres-server-main \
+      --restart unless-stopped \
       -e POSTGRES_USER="zabbix" \
       -e POSTGRES_PASSWORD="zabbix" \
-      -e POSTGRES_DB="zabbix" \
+      -e POSTGRES_DB="zabbix_pwd" \
       -v /var/lib/postgresql/docker/zabbix-main/data:/var/lib/postgresql/data \
       -v /etc/localtime:/etc/localtime:ro \
       -d postgres:11.2-alpine
 
-docker run -t --rm \
+docker run -t \
       --name zabbix-server-pgsql-main \
+      --restart unless-stopped \
       -e DB_SERVER_HOST="zabbix-postgres-server-main" \
       -e POSTGRES_USER="zabbix" \
       -e POSTGRES_PASSWORD="zabbix" \
-      -e POSTGRES_DB="zabbix" \
+      -e POSTGRES_DB="zabbix_pwd" \
       --link zabbix-postgres-server-main:postgres \
       -p 10051:10051 \
       -v /etc/localtime:/etc/localtime:ro \
       -d zabbix/zabbix-server-pgsql:latest
 
-docker run -t --rm \
+docker run -t \
       --name zabbix-web-nginx-pgsql-main \
+      --restart unless-stopped \
       -e DB_SERVER_HOST="zabbix-postgres-server-main" \
       -e POSTGRES_USER="zabbix" \
       -e POSTGRES_PASSWORD="zabbix" \
-      -e POSTGRES_DB="zabbix" \
+      -e POSTGRES_DB="zabbix_pwd" \
       -e ZBX_SERVER_NAME="Zabbix NSG" \
+      -e PHP_TZ="Europe/Moscow" \
       --link zabbix-postgres-server-main:postgres \
       --link zabbix-server-pgsql-main:zabbix-server \
       -p 80:80 \
