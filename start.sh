b9d6c4ede929482def0be2885441a160f06997c2
Igor Mosunov <imosunov@nsg.ru>
2019-05-23 12:28:46 +0300

    Start 3 servers

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker start zabbix-postgres-server zabbix-server-pgsql zabbix-web-nginx-pgsql acari-zabbix-agent
-docker start acari-server-db acari-server acari-client
+docker start acari-server-db acari-foo acari-bar acari-baz acari-client
